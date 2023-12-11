ab37dee469ed5d10b20d349e11ec09794f83b8a6
Aritra Sarkar <aritrasarkar2002@gmail.com>
2023-12-11 23:46:57 +0530

    styling in the table section

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -2,6 +2,23 @@
 import React, { useEffect, useState } from "react";
 import { Card, CardContent, CardHeader } from "./ui/card";
 import { useEthContext } from "@/context/eathContext";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import { Button } from "./ui/button";
+import { Grid } from "react-loader-spinner";
 
 type SearchResultProps = {
   accountAddress: string;
@@ -9,21 +26,20 @@ type SearchResultProps = {
 
 const SearchResult: React.FC<SearchResultProps> = ({ accountAddress }) => {
   const { ethPrice } = useEthContext();
-  const [balanceInWei, setBalanceInWei] = useState<string>("");
   const [balanceInEth, setBalanceInEth] = useState<string>("");
   const [transactions, setTransactions] = useState<any[]>([]);
-
+  const [loading, setLoading] = useState<boolean>(false);
   useEffect(() => {
     const fetchAccountData = async () => {
+      setLoading(true);
       try {
         const balanceResponse = await fetch(
           `https://api.etherscan.io/api?module=account&action=balance&address=${accountAddress}&apikey=11T9KQ5CKNTIJP92D7BIUJNNSS6WS9C823`
         );
         if (balanceResponse.ok) {
           const balanceData = await balanceResponse.json();
-          setBalanceInWei(balanceData.result);
-          const balanceInEth = parseFloat(balanceData.result) / 1e18; // Convert Wei to Ether
-          setBalanceInEth(balanceInEth.toFixed(5)); // Displaying 5 decimal places for Ether
+          const balanceInEth = parseFloat(balanceData.result) / 1e18;
+          setBalanceInEth(balanceInEth.toFixed(5));
         }
         const transactionsResponse = await fetch(
           `https://api.etherscan.io/api?module=account&action=txlist&address=${accountAddress}&apikey=11T9KQ5CKNTIJP92D7BIUJNNSS6WS9C823`
@@ -34,6 +50,8 @@ const SearchResult: React.FC<SearchResultProps> = ({ accountAddress }) => {
         }
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -41,6 +59,22 @@ const SearchResult: React.FC<SearchResultProps> = ({ accountAddress }) => {
       fetchAccountData();
     }
   }, [accountAddress]);
+
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const transactionsPerPage = 8;
+
+  const indexOfLastTransaction = currentPage * transactionsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
+  const currentTransactions = transactions.slice(
+    indexOfFirstTransaction,
+    indexOfLastTransaction
+  );
+
+  const totalPages = Math.ceil(transactions.length / transactionsPerPage);
+
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
 
   const formatEthPrice = (price: string) => {
     return parseFloat(price || "0").toFixed(2);
@@ -50,6 +84,13 @@ const SearchResult: React.FC<SearchResultProps> = ({ accountAddress }) => {
     const date = new Date(timestamp * 1000);
     return date.toISOString();
   };
+
+  const displayedPages = 10;
+  const maxDisplayedPages = Math.min(displayedPages, totalPages);
+  const startPage =
+    totalPages > maxDisplayedPages
+      ? Math.max(1, currentPage - Math.floor(maxDisplayedPages / 2))
+      : 1;
 
   return (
     <>
@@ -75,16 +116,109 @@ const SearchResult: React.FC<SearchResultProps> = ({ accountAddress }) => {
           <h3 className="px-4 py-2 bg-blue-500 text-white w-max rounded-lg">
             Transactions
           </h3>
-          <ul>
-            {transactions.map((transaction: any, index: number) => (
-              <li key={index}>
-                From: {transaction.from}, To: {transaction.to}, Value:{" "}
-                {transaction.value}, Hash: {transaction.hash}, Block:{" "}
-                {transaction.blockNumber}, Age:{" "}
-                {formatDate(transaction.timeStamp)}
-              </li>
-            ))}
-          </ul>
+          <Table className="border">
+            <TableCaption>List of all the transactions</TableCaption>
+            <TableHeader className="text-sm">
+              <TableRow>
+                <TableHead>Transaction Hash</TableHead>
+                <TableHead>Block</TableHead>
+                <TableHead>Age</TableHead>
+                <TableHead>From</TableHead>
+                <TableHead>To</TableHead>
+                <TableHead>Value</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {loading ? (
+                <div className="flex items-center justify-center py-8 w-full">
+                  <Grid
+                    height="80"
+                    width="80"
+                    color="#4F6CFF"
+                    ariaLabel="grid-loading"
+                    radius="12.5"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                  />
+                </div>
+              ) : (
+                currentTransactions.map((transaction: any, index: number) => (
+                  <TooltipProvider>
+                    <TableRow key={index}>
+                      <TableCell>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            {transaction.hash
+                              ? `${transaction.hash.substring(0, 16)}...`
+                              : "Loading..."}
+                          </TooltipTrigger>
+                          <TooltipContent>{transaction.hash}</TooltipContent>
+                        </Tooltip>
+                      </TableCell>
+                      <TableCell>{transaction.blockNumber}</TableCell>
+                      <TableCell>{formatDate(transaction.timeStamp)}</TableCell>
+                      <TableCell>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            {transaction.from
+                              ? `${transaction.from.substring(0, 16)}...`
+                              : "Loading..."}
+                          </TooltipTrigger>
+                          <TooltipContent>{transaction.from}</TooltipContent>
+                        </Tooltip>
+                      </TableCell>
+                      <TableCell>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            {transaction.to
+                              ? `${transaction.to.substring(0, 16)}...`
+                              : "Loading..."}
+                          </TooltipTrigger>
+                          <TooltipContent>{transaction.to}</TooltipContent>
+                        </Tooltip>
+                      </TableCell>
+                      <TableCell>{transaction.value}</TableCell>
+                    </TableRow>
+                  </TooltipProvider>
+                ))
+              )}
+            </TableBody>
+          </Table>
+          {transactions.length > transactionsPerPage && (
+            <div className="flex justify-center space-x-2 overflow-auto">
+              {currentPage > 1 && (
+                <Button
+                  onClick={() => paginate(currentPage - 1)}
+                  disabled={currentPage === 1}
+                >
+                  Previous
+                </Button>
+              )}
+              {Array.from(
+                { length: maxDisplayedPages },
+                (_, i) => startPage + i
+              ).map((page) => (
+                <Button
+                  key={page}
+                  onClick={() => paginate(page)}
+                  className={
+                    currentPage === page
+                      ? "bg-primary text-primary-foreground hover:bg-primary/90"
+                      : "bg-secondary text-secondary-foreground hover:bg-secondary/80"
+                  }
+                >
+                  {page}
+                </Button>
+              ))}
+              <Button
+                onClick={() => paginate(currentPage + 1)}
+                disabled={currentPage === totalPages}
+              >
+                Next
+              </Button>
+            </div>
+          )}
         </div>
       ) : (
         <></>
